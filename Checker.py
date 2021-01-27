daa4a2b0a9b3bfd3f5f7108132e2b01552cacdd1
K4Sp3r <77979875+80i@users.noreply.github.com>
2021-01-27 14:22:30 +0300

    Update Checker.py

--- Checker.py
+++ Checker.py
@@ -6,6 +6,7 @@ os.system("cls" or "clear")
 print(Fore.RED+"""
 
 Discord Server :  https://discord.gg/eJeHG7D6pm
+   My Discord : @K4So3r#1337
    _____ _               _                       __ 
   / ____| |             | |                     /_ |
  | |    | |__   ___  ___| | _____ _ __     __   _| |
@@ -26,13 +27,13 @@ print("")
 web = list(websit)
 webs = web[:-1]
 webss = ''.join(webs)
-wbhook = str(input("Webhook : "))
+#wbhook = str(input("Webhook : "))
 print("")
 time.sleep(.3)
 print(" ")
 while True:
     usernames = ('').join(random.choices(string.ascii_lowercase + string.digits, k=hmletters))
-    webhook = DiscordWebhook(url=wbhook, content=f'`{usernames}` | Might be Available or Banned on => ||`{webss}`|| |')
+    #webhook = DiscordWebhook(url=wbhook, content=f'`{usernames}` | Might be Available or Banned on => ||`{webss}`|| |')
     r = requests.get(f'https://{websit}{usernames}')
     if r.status_code == 200:
         print(Fore.CYAN+"[-] "+Fore.RED + "UnAvailable"+ Fore.WHITE +' |=>'+Fore.YELLOW+ f' {usernames}'+Fore.WHITE+" <=|"+Fore.CYAN+" [-]")
@@ -40,4 +41,4 @@ while True:
         print(Fore.CYAN + "[+] " + Fore.GREEN + "Available" + Fore.WHITE + ' |=>' + Fore.LIGHTMAGENTA_EX + f' {usernames}'+Fore.WHITE+" <=|" + Fore.CYAN + " [+]")
         f = open("availables.txt", "a", encoding='utf-8')
         f.write(f"{usernames} | Might be Available or Banned on => {webss} |\n")
-        webhook.execute()
+        #webhook.execute()
